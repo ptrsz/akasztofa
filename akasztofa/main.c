e4fecf1053855e16a2f99079c1a23a7b5c29ec49
Peter Szombathelyi <szombat@dhcp-141.r.wlan.net.bme.hu>
2012-10-24 17:23:50 +0200

    Initial Commit

---
+++ akasztofa/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  akasztofa
+//
+//  Created by Peter Szombathelyi on 2012.10.24..
+//  Copyright (c) 2012 Peter Szombathelyi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
